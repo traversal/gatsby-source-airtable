6b2dbb31c9f23c331c48981a9e220c83a83c3918
Travis Hensgen <travishensgen@gmail.com>
2019-05-15 14:36:33 +1000

    update to latest source and redo changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,21 @@
 const Airtable = require("airtable");
 const crypto = require(`crypto`);
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
+const { map } = require('bluebird');
 
 exports.sourceNodes = async (
   { actions, createNodeId, store, cache },
-  { apiKey, tables, rawValues }
+  { apiKey, tables, concurrency, rawValues }
 ) => {
   // tables contain baseId, tableName, tableView, queryName, mapping, tableLinks
   const { createNode, setPluginStatus } = actions;
 
   try {
     // hoist api so we can use in scope outside of this block
-    var api = await new Airtable({
-      apiKey: process.env.GATSBY_AIRTABLE_API_KEY || apiKey
-    });
+    if (!apiKey && process.env.GATSBY_AIRTABLE_API_KEY) {
+      console.warn("\nImplicit setting of GATSBY_AIRTABLE_API_KEY as apiKey will be deprecated in future release, apiKey should be set in gatsby-config.js, please see Readme!")
+    }
+    var api = await new Airtable({ apiKey: process.env.GATSBY_AIRTABLE_API_KEY || apiKey });
   } catch (e) {
     // airtable uses `assert` which doesn't exit the process,
     //  but rather just makes gatsby hang. Warn, don't create any
@@ -30,6 +32,14 @@ exports.sourceNodes = async (
     return;
   }
 
+  if (concurrency === undefined) {
+    // Airtable hasn't documented what the rate limit against their attachment servers is. 
+    // They do document that API calls are limited to 5 requests/sec, so the default limit of 5 concurrent 
+    // requests for remote files has been selected in that spirit. A higher value can be set as a plugin 
+    // option in gatsby-config.js
+    concurrency = 5;
+  }
+
   console.time(`\nfetch all Airtable rows from ${tables.length} tables`);
 
   let queue = [];
@@ -44,6 +54,37 @@ exports.sourceNodes = async (
       view: view
     });
 
+    // confirm that the user is using the clean keys
+    // if they are not, warn them and change it for them
+    // we can settle the API on clean keys and not have a breaking
+    // change until the next major version when we remove this
+    const cleanMapping = !tableOptions.mapping ? null : Object.keys(tableOptions.mapping).reduce((cleaned, key) => {
+      let useKey = cleanKey(key)
+      if (useKey !== key) console.warn(`
+        Field names within graphql cannot have spaces. We do not want you to change your column names
+        within Airtable, but in "Gatsby-land" you will need to always use the "cleaned" key.
+        On the ${tableOptions.tableName} base ${tableOptions.baseId} 'mapping', we modified the supplied key of
+        ${key} to instead be ${useKey}. Please use ${useKey} in all of your queries. Also, update your config
+        to use ${useKey} to make this warning go away. See https://github.com/jbolda/gatsby-source-airtable#column-names
+        for more information.
+      `)
+      cleaned[useKey] = tableOptions.mapping[key]
+      return cleaned
+    }, {})
+
+    const cleanLinks = !tableOptions.tableLinks ? null : tableOptions.tableLinks.map(key => {
+      let useKey = cleanKey(key)
+      if (useKey !== key) console.warn(`
+        Field names within graphql cannot have spaces. We do not want you to change your column names
+        within Airtable, but in "Gatsby-land" you will need to always use the "cleaned" key.
+        On the ${tableOptions.tableName} base ${tableOptions.baseId} 'tableLinks', we modified the supplied key of
+        ${key} to instead be ${useKey}. Please use ${useKey} in all of your queries. Also, update your config
+        to use ${useKey} to make this warning go away. See https://github.com/jbolda/gatsby-source-airtable#column-names
+        for more information.
+      `)
+      return useKey
+    })
+
     // query.all() returns a promise, pass an array for each table with
     // both our promise and the queryName and then map reduce at the
     // final promise resolution to get queryName onto each row
@@ -51,8 +92,9 @@ exports.sourceNodes = async (
       Promise.all([
         query.all(),
         tableOptions.queryName,
-        tableOptions.mapping,
-        tableOptions.tableLinks,
+        tableOptions.defaultValues || {},
+        cleanMapping,
+        cleanLinks,
         tableOptions.tableName,
         cleanTypeName(tableOptions.typeName || 'Airtable'),
         tableOptions.rawValues,
@@ -69,13 +111,15 @@ exports.sourceNodes = async (
         return accumulator.concat(
           currentValue[0].map(row => {
             row.queryName = currentValue[1]; // queryName from tableOptions above
-            row.mapping = currentValue[2]; // mapping from tableOptions above
-            row.tableLinks = currentValue[3]; // tableLinks from tableOptions above
-            row.tableName = currentValue[4]; // tableName from tableOptions above
-            row.typeName = currentValue[5]; // typeName from tableOptions above
+            row.defaultValues = currentValue[2]; // mapping from tableOptions above
+            row.mapping = currentValue[3]; // mapping from tableOptions above
+            row.tableLinks = currentValue[4]; // tableLinks from tableOptions above
+            row.tableName = currentValue[5]; // tableName from tableOptions above
+            row.typeName = currentValue[6]; // typeName from tableOptions above
             // store raw values by default, but allow them to be switched off globally or on a table-by-table basis
             // they often cause warnings about GraphQL type conflicts with Gatsby's auto-type-inference 
-            row.rawValues = rawValues !== false && currentValue[5] !== false; 
+            row.rawValues = rawValues !== false && currentValue[7] !== false; 
+
             return row;
           })
         );
@@ -94,19 +138,30 @@ exports.sourceNodes = async (
     }
   });
 
-  let childNodes = allRows.map(async row => {
-
+  // Use the map function for arrays of promises imported from Bluebird.
+  // Using the concurrency option protects against being blocked from Airtable's
+  // file attachment servers for large numbers of requests.
+  return map(allRows, async row => {
+    // don't love mutating the row here, but
+    // not ready to refactor yet to clean this up
+    // (happy to take a PR!)
+    row.fields = {
+      ...row.defaultValues,
+      ...row.fields,
+    }
     let processedData = await processData(row, {
       createNodeId,
       createNode,
       store,
-      cache,
+      cache
     });
-    
+
     const node = {
       id: createNodeId(`Airtable_${row.id}`),
       parent: null,
       table: row.tableName,
+
+      recordId: row.id,
       queryName: row.queryName,
       children: [],
       internal: {
@@ -120,36 +175,38 @@ exports.sourceNodes = async (
     };
 
     createNode(node);
-    return processedData.childNodes;
-  });
 
-  let flattenedChildNodes = Promise.all(childNodes).then(nodes =>
-    nodes.reduce(
-      (accumulator, currentValue) => accumulator.concat(currentValue),
-      []
-    )
-  );
-
-  return Promise.all(flattenedChildNodes).then(nodes => {
-    nodes.forEach(node => createNode(node));
-  });
+    await Promise.all(processedData.childNodes).then(nodes => {
+      nodes.forEach(node => createNode(node));
+    });
+  }, { concurrency: concurrency });
 };
 
-const processData = async (row, { createNodeId, createNode, store, cache }) => {
+const processData = async (
+  row,
+  { createNodeId, createNode, store, cache }
+) => {
   let data = row.fields;
   let tableLinks = row.tableLinks;
-
   let fieldKeys = Object.keys(data);
   let processedData = {};
   let childNodes = [];
 
   await fieldKeys.forEach(key => {
+    // once in "Gatsby-land" we want to use the cleanKey
+    // consistently everywhere including in configs
+    // this key that we clean comes from Airtable
+    // at this point, all user option keys should be clean
+    const cleanedKey = cleanKey(key)
+
     let useKey;
     // deals with airtable linked fields,
     // these will be airtable IDs
-    if (tableLinks && tableLinks.includes(key)) {
-      useKey = `${cleanKey(key)}___NODE`;
+    if (tableLinks && tableLinks.includes(cleanedKey)) {
+      useKey = `${cleanedKey}___NODE`;
 
+      // `data` is direct from Airtable so we don't use
+      // the cleanKey here
       try {
         processedData[useKey] = data[key].map(id =>
           createNodeId(`Airtable_${id}`)
@@ -163,23 +220,25 @@ const processData = async (row, { createNodeId, createNode, store, cache }) => {
           otherwise this may cause unexpected problems with your build or site.
         `);
       }
-      
+
+    } else if (row.mapping && row.mapping[cleanedKey]) {
       // A child node comes from the mapping, where we want to
       // define a separate node in gatsby that is available
       // for transforming. This will let other plugins pick
       // up on that node to add nodes.
+      // row contains `data` which is direct from Airtable
+      // so we pass down the raw instead of the cleanKey here
+      let checkedChildNode = checkChildNode(key, row, processedData, {
+        createNodeId,
+        createNode,
+        store,
+        cache
+      });
+      childNodes.push(checkedChildNode);
     } else {
-      if (row.mapping && row.mapping[key]) {
-        let checkedChildNode = checkChildNode(key, row, processedData, {
-          createNodeId,
-          createNode,
-          store,
-          cache,
-        });
-        childNodes.push(checkedChildNode);
-      } else {
-        processedData[cleanKey(key)] = data[key];
-      }
+      // `data` is direct from Airtable so we don't use
+      // the cleanKey here
+      processedData[cleanedKey] = data[key];
     }
   });
 
@@ -213,7 +272,7 @@ const checkChildNode = async (
     cleanedKey,
     data[key],
     mapping[key],
-    createNodeId,
+    createNodeId
   );
 };
 
@@ -224,10 +283,13 @@ const localFileCheck = async (
 ) => {
   let data = row.fields;
   let mapping = row.mapping;
-  if (mapping[key] === `fileNode`) {
+  let cleanedKey = cleanKey(key);
+  if (mapping[cleanedKey] === `fileNode`) {
     try {
       let fileNodes = [];
       // where data[key] is the array of attachments
+      // `data` is direct from Airtable so we don't use
+      // the cleanKey here
       data[key].forEach(attachment => {
         let attachmentNode = createRemoteFileNode({
           url: attachment.url,
@@ -240,7 +302,8 @@ const localFileCheck = async (
       });
       // Adds a field `localFile` to the node
       // ___NODE tells Gatsby that this field will link to another nodes
-      let localFiles = await Promise.all(fileNodes).map(
+      const resolvedFileNodes = await Promise.all(fileNodes);
+      const localFiles = resolvedFileNodes.map(
         attachmentNode => attachmentNode.id
       );
       return localFiles;
@@ -265,7 +328,7 @@ const buildNode = (localFiles, row, cleanedKey, raw, mapping, createNodeId) => {
       internal: {
         type: `${row.typeName}Field`,
         mediaType: mapping,
-        content: typeof raw === 'string' ? raw : JSON.stringify(raw),
+        content: typeof raw === "string" ? raw : JSON.stringify(raw),
         contentDigest: crypto
           .createHash("md5")
           .update(JSON.stringify(row))
@@ -277,11 +340,11 @@ const buildNode = (localFiles, row, cleanedKey, raw, mapping, createNodeId) => {
       id: createNodeId(`AirtableField_${row.id}_${cleanedKey}`),
       parent: createNodeId(`Airtable_${row.id}`),
       children: [],
-      raw: row.rawValues ? raw : null,
+      raw: raw,
       internal: {
-        type: `${row.typeName}Field`,
+        type: `AirtableField`,
         mediaType: mapping,
-        content: typeof raw === 'string' ? raw : JSON.stringify(raw),
+        content: typeof raw === "string" ? raw : JSON.stringify(raw),
         contentDigest: crypto
           .createHash("md5")
           .update(JSON.stringify(row))
@@ -303,3 +366,4 @@ const upperFirst = (str) => {
 const cleanTypeName = (typeName) => {
   return upperFirst(typeName.replace(/[^a-z]/ig, ''));
 };
+
